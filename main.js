6bab289cb45056e5537fbd8a758876ed2de28b8a
AngieIannopollo007 <167256745+AngieIannopollo007@users.noreply.github.com>
2025-05-12 18:20:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+const total = 150; 
+let estado = JSON.parse(localStorage.getItem("album")) || Array(total).fill(false); 
+const contenedor = document.getElementById("contenedor"); 
+const contador = document.getElementById("contador"); 
+
+function actualizarContador() {
+    let faltan = 0;
+    for (let i = 0; i < estado.length; i++) {
+        if (estado[i] === false) {
+            faltan++;
+        }
+    }
+    contador.textContent = `Faltan ${faltan} figuritas`;
+}
+
+ function guardar() { 
+    localStorage.setItem("album", JSON.stringify(estado)); 
+
+}
+ for (let i = 0; i < total; i++) { 
+    const btn = document.createElement("button"); 
+    btn.className = "boton"; 
+    btn.textContent = i + 1;
+
+     if (estado[i]) { 
+        btn.classList.add("completa"); 
+     } 
+
+     btn.addEventListener("click", () => {
+        estado[i] = !estado[i];
+        btn.classList.toggle("completa");
+        actualizarContador();
+        guardar();
+    });
+    
+     contenedor.appendChild(btn); 
+    }
+
+     actualizarContador(); 
